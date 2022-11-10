26ab70351e8a47ddb850b7d9b47a2802b0fa9df9
Cristina Kukhar <criskukhar@hotmail.com>
2022-11-10 21:35:54 +0100

    basic form layout and components for NewAdvert

--- src/components/adverts/NewAdvert.js
+++ src/components/adverts/NewAdvert.js
@@ -1,9 +1,31 @@
 import Layout from "../layout/Layout"
+import { Checkbox, FormField } from "./auth/FormField"
 
 const NewAd = props => {
     return(
         <Layout title='Create your advert' {...props}>
-            <p> New Advert Page</p>
+            <form>
+                <FormField
+                label='Name'/>
+                <FormField
+                label='Price'/>
+                <Checkbox
+                label='sale'/>
+                <Checkbox
+                label='search'/>
+                <select value='tags'>
+                    <option value='lifestyle'>Lifestyle</option>
+                    <option value='mobile'>Mobile</option>
+                    <option value='motor'>Motor</option>
+                    <option value='work'>Work</option>
+                </select>
+                <input
+                type="file"
+                onChange={event => console.log(event.target.files)}/>
+                <button 
+                type="submit" 
+                > Post Advert </button>
+            </form>
         </Layout>
     ) 
 }
